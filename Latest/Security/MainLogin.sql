7b2e8de270171e1110c79ab655b5c28bc3b3f9f1
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2025-04-22 22:31:13 +0200

    Hauptbenutzer angelegt

---
+++ Latest/Security/MainLogin.sql
@@ -0,0 +1,4 @@
+﻿-- Create Main Login for the Connection to the database.
+-- It is strongly recommended to change the password after first connection to the database.
+
+CREATE LOGIN [WebControl] WITH PASSWORD = 'WebControl1234', CHECK_EXPIRATION = OFF, CHECK_POLICY = OFF;
---
+++ Latest/Security/MainUser.sql
@@ -0,0 +1,8 @@
+﻿-- Create Main User for the connection to the database.
+
+CREATE USER [Demo]
+FOR LOGIN [WebControl]
+WITH DEFAULT_SCHEMA = dbo;
+GO
+
+GRANT CONNECT TO [Demo]
