a582de023a7a6a2e283619bccb1f9cd80aa3a609
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2025-04-26 00:28:09 +0200

    Users Tabelle erstellt

---
+++ Latest/dbo/Tables/Users.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Users] (
+	[ID] INT IDENTITY (1, 1) NOT NULL
+   ,[LoginName] VARCHAR(50) NOT NULL
+   ,[LoginPwd] VARBINARY(MAX) NOT NULL
+   ,[FirstName] VARCHAR(50) NULL
+   ,[LastName] VARCHAR(50) NULL
+   ,[Department] VARCHAR(50) NULL
+   ,[LoginActive] BIT NOT NULL
+   ,[LastSuccesfulLogin] DATETIME2 NULL
+   ,[LastFailedLogin] DATETIME2 NULL
+   ,[FailedLoginCounter] SMALLINT NOT NULL CONSTRAINT [DF_Users_FailedLoginCounter] DEFAULT 0
+   ,CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([ID]) ON [DEFAULT]
+   ,INDEX [IX_Users] UNIQUE NONCLUSTERED ([LoginName] ASC) ON [DEFAULT]
+) ON [DEFAULT];
