71d80798e4cbb2a867f5ef2a9738d466b4fa6fa4
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2025-04-29 16:49:22 +0200

    Trigger für Users ergänzt

---
+++ Latest/dbo/Triggers/Users_BlockDirect.sql
@@ -0,0 +1,18 @@
+﻿-- This trigger blocks all direct transactions to the users-table.
+-- It is only allowed to modify the table by procedures.
+
+CREATE TRIGGER [Users_BlockDirect]
+ON [dbo].[Users]
+FOR DELETE, INSERT, UPDATE
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	IF @@NESTLEVEL <= 1
+	BEGIN
+		IF @@TRANCOUNT > 0
+			ROLLBACK;
+		THROW 50004, 'No direct modifications allowed on the users-table!', 1;
+
+	END;
+END;
