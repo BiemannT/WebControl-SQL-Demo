4b9579f0cf3a1c375e341e51d60fe032df7a41ac
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2025-05-01 11:20:38 +0200

    Update Users.sql
    DEFAULT Constraint umgeschrieben

--- Latest/dbo/Tables/Users.sql
+++ Latest/dbo/Tables/Users.sql
@@ -8,7 +8,7 @@
    ,[LoginActive] BIT NOT NULL
    ,[LastSuccesfulLogin] DATETIME2(2) NULL
    ,[LastFailedLogin] DATETIME2(2) NULL
-   ,[FailedLoginCounter] SMALLINT NOT NULL CONSTRAINT [DF_Users_FailedLoginCounter] DEFAULT 0
+   ,[FailedLoginCounter] SMALLINT CONSTRAINT [DF_Users_FailedLoginCounter] DEFAULT 0 NOT NULL
    ,[LoginBlocked] AS IIF([FailedLoginCounter] > 3, 1, 0)
    ,CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([ID]) ON [DEFAULT]
    ,INDEX [IX_Users] UNIQUE NONCLUSTERED ([LoginName] ASC) ON [DEFAULT]
