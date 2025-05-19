f9a7e9b252e798849ba0f7d9ac11caa3c75ce880
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2025-05-19 23:36:54 +0200

    Versionstabelle erstellt

---
+++ Latest/dbo/Tables/VersionHistory.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[VersionHistory] (
+	[MajorVers] TINYINT NOT NULL
+   ,[MinorVers] TINYINT NOT NULL
+   ,[PatchVers] TINYINT NOT NULL
+   ,[InstallDate] DATETIME2(3) CONSTRAINT [DF_VersionHistory_InstallDate] DEFAULT GETDATE() NOT NULL
+) ON [DEFAULT];
