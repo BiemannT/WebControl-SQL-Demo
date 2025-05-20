1ae9b651d5f19fd7f42e9a622b991026b9c14b11
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2025-05-20 23:14:58 +0200

    Prozedur SEL_VersionHistory_Latest ergänzt

---
+++ Latest/dbo/Stored Procedures/SEL_VersionHistory_Latest.sql
@@ -0,0 +1,17 @@
+﻿-- Ruft die zuletzt installierte DACPAC-Version ab.
+
+CREATE PROCEDURE [dbo].[SEL_VersionHistory_Latest]
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT TOP (1)
+		[vh].[MajorVers]
+	   ,[vh].[MinorVers]
+	   ,[vh].[PatchVers]
+	   ,[vh].[InstallDate]
+
+	FROM [dbo].[VersionHistory] [vh]
+	ORDER BY [vh].[InstallDate] DESC;
+
+END;
