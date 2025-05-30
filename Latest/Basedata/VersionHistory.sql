11c5690707e9dd7ab7dec61b8280f1ad75036a32
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2025-05-30 21:50:06 +0200

    Anpassung Vor und Nach Skript

--- Latest/Basedata/VersionHistory.sql
+++
@@ -1,4 +0,0 @@
-﻿-- Write current DACPAC-version into the table VersionHistory:
-
-INSERT INTO [dbo].[VersionHistory] ([MajorVers], [MinorVers], [PatchVers])
-	VALUES (1, 1, 0);
--- Latest/PD.sql
+++ Latest/PD.sql
@@ -1,21 +1,43 @@
-﻿/*
-Vorlage für ein Skript nach der Bereitstellung							
---------------------------------------------------------------------------------------
- Diese Datei enthält SQL-Anweisungen, die an das Buildskript angefügt werden.		
- Schließen Sie mit der SQLCMD-Syntax eine Datei in das Skript nach der Bereitstellung ein.			
- Beispiel:   :r .\myfile.sql								
- Verwenden Sie die SQLCMD-Syntax, um auf eine Variable im Skript nach der Bereitstellung zu verweisen.		
- Beispiel:   :setvar TableName MyTable							
-               SELECT * FROM [$(TableName)]					
---------------------------------------------------------------------------------------
-*/
+﻿SET XACT_ABORT ON;
+-- Old version number
+DECLARE @OldMajorVersion AS TINYINT;
+DECLARE @OldMinorVersion AS TINYINT;
+DECLARE @OldPatchVersion AS TINYINT;
 
-SET XACT_ABORT ON;
+SELECT TOP (1)
+	@OldMajorVersion = [vh].[MajorVersion]
+   ,@OldMinorVersion = [vh].[MinorVersion]
+   ,@OldPatchVersion = [vh].[PatchVersion]
+FROM [dbo].[VersionHistory] [vh]
+ORDER BY [vh].[InstallDate] DESC;
 
--- Fill Base-Tables
+-- New version number
+DECLARE @NewMajorVersion AS TINYINT;
+DECLARE @NewMinorVersion AS TINYINT;
+DECLARE @NewPatchVersion AS TINYINT;
+
+SET @NewMajorVersion = 1;
+SET @NewMinorVersion = 2;
+SET @NewPatchVersion = 0;
+
+-- Write Basic-Data
 
 -- Errors-Table
-:R ..\Latest\Basedata\Errors.sql
+:R..\Latest\Basedata\Errors.sql
+
+-- Database post-deploy update-script
+-- The pre-check of the required minimum version is already done in the pre-deployment script.
+-- Perform only the update-script, if an installation already exists with an older version.
+IF ISNULL(@OldMajorVersion, @NewMajorVersion) <= @NewMajorVersion
+	AND ISNULL(@OldMinorVersion, @NewMinorVersion) <= @NewMinorVersion
+	AND ISNULL(@OldPatchVersion, @NewPatchVersion) < @NewPatchVersion
+BEGIN
+	PRINT 'Execute update script...';
+
+	-- R: ..\Latest\Updates\xyz.sql
+
+END;
 
 -- Write current Databaseversion into the table VersionHistory
-:R ..\Latest\Basedata\VersionHistory.sql
+INSERT INTO [dbo].[VersionHistory] ([MajorVers], [MinorVers], [PatchVers])
+	VALUES (@NewMajorVersion, @NewMinorVersion, @NewPatchVersion);
